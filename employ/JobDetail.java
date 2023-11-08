57a84565060c2c94e25789e33c06d02533d503e4
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-07 20:00:00 -0500

    Create 'JobDetail.java' class

---
+++ employ/JobDetail.java
@@ -0,0 +1,16 @@
+package employ;
+
+public class JobDetail {
+
+ public String job_id;
+ public String name;
+ public String father_name;
+ public String job_contact;
+ public String email;
+ public String salary;
+ public String position;
+
+ public void getInfo() {
+ }
+
+}
