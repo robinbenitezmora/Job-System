46be99e865c75a301f3800bb8c4126469035e02f
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-07 19:56:38 -0500

    Create 'AddJob.java' class

--- employ/JobSystem.java
+++ employ/JobSystem.java
@@ -1,5 +1,10 @@
 package employ;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.Scanner;
+
 /******************************** MENU ****************************/
 
 class MainMenu {
@@ -16,4 +21,40 @@ class MainMenu {
   System.out.println(" Click on 4: To Update Job Details");
   System.out.println(" Click on 5: To Exit");
  }
+}
+
+/************************ To add details of the Job *********************/
+
+class AddJob {
+ public void createFile() {
+  Scanner sc = Scanner(System.in);
+
+  JobDetail job = new JobDetail();
+  job.getInfo();
+
+  try {
+   File file = new File("file" + job.job_id + ".txt");
+   if (file.createNewFile()) {
+    FileWriter myWriter = new FileWriter("file" + job.job_id + ".txt");
+    myWriter.write("Employee ID: " + job.job_id + "\n" + "Employee Name: " + job.name + "\n" + "Father's Name: "
+      + job.father_name + "\n" + "Employee Contact: " + job.job_contact + "\n" + "Email Information: " + job.email
+      + "\n" + "Employee Position: " + job.position + "\n" + "Employee Salary: " + job.salary);
+    myWriter.close();
+    System.out.println("\nEmployee has been Created: )\n");
+
+    System.out.println("\nPress Enter to Continue...");
+    sc.nextLine();
+   } else {
+    System.out.println("\nEmployee already exists.\n");
+   }
+  } catch (Exception e) {
+   System.out.println("An error occurred.");
+   e.printStackTrace();
+  }
+ }
+
+ private Scanner Scanner(InputStream in) {
+  return null;
+ }
+
 }
