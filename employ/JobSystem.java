b064b108ff5f0541dfa80d74ab9176fb8b35114f
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-07 19:30:18 -0500

    Create the 'MainMenu.java' class

---
+++ employ/JobSystem.java
@@ -0,0 +1,19 @@
+package employ;
+
+/******************************** MENU ****************************/
+
+class MainMenu {
+ public void menu() {
+  System.out.println("\t\t*******************************************");
+  System.out.println("\t\t\t  JOB SYSTEM");
+  System.out.println("\t\t*******************************************");
+  System.out.println("\t\t\t    --------------------");
+  System.out.println("\t\t\t     ~$ Robin Benitez");
+  System.out.println("\t\t\t    --------------------");
+  System.out.println("\n\n Click on 1: To Add a Job Details");
+  System.out.println(" Click on 2: To View Job Details");
+  System.out.println(" Click on 3: To Delete Job Details");
+  System.out.println(" Click on 4: To Update Job Details");
+  System.out.println(" Click on 5: To Exit");
+ }
+}
